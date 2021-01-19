c52e7c41f1c432f501065867d2253feada13018a
shutoushu <56241683+shutoushu@users.noreply.github.com>
2021-01-19 22:09:19 +0900

    追加ファイル出力

--- src/obstacle/model/obstacle-shadowing-propagation-loss-model.cc
+++ src/obstacle/model/obstacle-shadowing-propagation-loss-model.cc
@@ -89,7 +89,7 @@ ObstacleShadowingPropagationLossModel::GetLoss (Ptr<MobilityModel> a, Ptr<Mobili
     }
   // std::cout<<"L_obs の値は"<<L_obs<<"\n";
   // std::cout<<"L_obs"<<L_obs<<"\n";
-  L_obs = L_obs * 2.3;
+  L_obs = L_obs * 10;
 
   return L_obs;
 }
