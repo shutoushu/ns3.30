725291127f9d989aa9130e15d2472d7f3ef7991e
shutoushu <56241683+shutoushu@users.noreply.github.com>
2021-02-14 15:48:42 +0900

    shadowing test before code status

---
+++ src/wave/examples/LSGO_Grid/test.tcl
@@ -0,0 +1,32 @@
+$node_(0) set X_ 1212.3
+$node_(0) set Y_ 898.4
+$node_(0) set Z_ 0
+$ns_ at 0.0 "$node_(0) setdest 1212.3 898.4 0.00"
+$ns_ at 1.0 "$node_(0) setdest 1213.93 898.4 1.63"
+$node_(1) set X_ 2087.7
+$node_(1) set Y_ 301.6
+$node_(1) set Z_ 0
+$ns_ at 1.0 "$node_(1) setdest 2087.7 301.6 0.00"
+$node_(2) set X_ 898.4
+$node_(2) set Y_ 1787.7
+$node_(2) set Z_ 0
+$ns_ at 1.0 "$node_(2) setdest 898.4 1787.7 0.00"
+$node_(3) set X_ 901.6
+$node_(3) set Y_ 1212.3
+$node_(3) set Z_ 0
+$ns_ at 1.0 "$node_(3) setdest 901.6 1212.3 0.00"
+$node_(4) set X_ 1201.6
+$node_(4) set Y_ 612.3
+$node_(4) set Z_ 0
+$ns_ at 1.0 "$node_(4) setdest 1201.6 612.3 0.00"
+$node_(5) set X_ 898.4
+$node_(5) set Y_ 887.7
+$node_(5) set Z_ 0
+$ns_ at 1.0 "$node_(5) setdest 898.4 887.7 0.00"
+$node_(6) set X_ 2101.6
+$node_(6) set Y_ 1212.3
+$node_(6) set Z_ 0
+$ns_ at 1.0 "$node_(6) setdest 2101.6 1212.3 0.00"
+$node_(7) set X_ 1798.4
+$node_(7) set Y_ 287.7
+$node_(7) set Z_ 0
