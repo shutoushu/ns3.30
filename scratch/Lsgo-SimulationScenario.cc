3d38c55ccd70f39ea4804ed286c04d13f43c79bf
shutoushu <56241683+shutoushu@users.noreply.github.com>
2021-04-05 00:02:18 +0900

    seed 1100 nodenum 900 add

--- scratch/Lsgo-SimulationScenario.cc
+++ scratch/Lsgo-SimulationScenario.cc
@@ -2471,7 +2471,7 @@ VanetRoutingExperiment::SetupScenario ()
 
       m_mobility = 1;
       m_nNodes = 900; //変更すべきところ
-      m_TotalSimTime = 40; //変更すべきところ
+      m_TotalSimTime = 23; //変更すべきところ
       m_nodeSpeed = 0;
       m_nodePause = 0;
       m_CSVfileName = "Raleigh_Downtown50_vanet-routing-compare.csv";
